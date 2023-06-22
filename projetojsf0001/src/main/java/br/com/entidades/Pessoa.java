a8ce2c690bd39440a7c5935e77755ea750fd5bbc
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-06-22 18:04:48 -0300

    adcionado login, senha, perfil e alterado botão

--- projetojsf0001/src/main/java/br/com/entidades/Pessoa.java
+++ projetojsf0001/src/main/java/br/com/entidades/Pessoa.java
@@ -33,6 +33,12 @@ public class Pessoa implements Serializable {
 	
 	private Boolean ativo;
 	
+	private String login;
+	
+	private String senha;
+	
+	private String perfilUser;
+	
 	
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
@@ -100,6 +106,30 @@ public class Pessoa implements Serializable {
 
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getPerfilUser() {
+		return perfilUser;
+	}
+	
+	public void setPerfilUser(String perfilUser) {
+		this.perfilUser = perfilUser;
 	}
 
 	@Override
