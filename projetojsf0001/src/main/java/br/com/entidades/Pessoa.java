b6903ec3245ccfa9524de1b2f58c6a27a816988d
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-06-22 15:41:13 -0300

    adicionado selectOneMenu para campo sexo e adicionado imagem

--- projetojsf0001/src/main/java/br/com/entidades/Pessoa.java
+++ projetojsf0001/src/main/java/br/com/entidades/Pessoa.java
@@ -27,6 +27,8 @@ public class Pessoa implements Serializable {
 	
 	private Integer idade;
 	
+	private String sexo;
+	
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
 
@@ -68,6 +70,14 @@ public class Pessoa implements Serializable {
 
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
 	}
 
 	@Override
