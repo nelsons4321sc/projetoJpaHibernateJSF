cc102e4d49fdd22cf24fc7e5f955c56685fa2258
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-06-29 14:06:14 -0300

    adcionado mensagem de salvar e remover

--- projetojsf0001/src/main/java/br/com/jsf/PessoaBean.java
+++ projetojsf0001/src/main/java/br/com/jsf/PessoaBean.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
@@ -28,9 +29,17 @@ public class PessoaBean  {
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
 		carregarPessoas();
+		mostrarmsg("cadastrado com sucesso");
 		return "";
 	}
 	
+	private void mostrarmsg(String msg) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		FacesMessage message = new FacesMessage(msg);
+		context.addMessage(null, message);
+		
+	}
+
 	public String novo() {
 		pessoa = new Pessoa();
 		return "";
@@ -40,6 +49,7 @@ public class PessoaBean  {
 		daoGeneric.deletePorId(pessoa);
 		pessoa = new Pessoa();
 		carregarPessoas();
+		mostrarmsg("Removido com Sucesso");
 		return "";
 	}
 	
